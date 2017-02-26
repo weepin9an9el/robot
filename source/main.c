03debf5464ac2522e65ae162518b5885cbe5cfab
weepin9an9el <weepin9an9el@gmail.com>
2017-02-26 16:15:25 -0600

    Added readings for both wheels and changed servo timings so that wheels move based on object detection - trial 1!!!!!!

--- source/main.c
+++ source/main.c
@@ -6,9 +6,8 @@
 
 void main(void)
 {
-float lresult;
-//float rresult;
-
+int lresult;
+int rresult;
 
 initADC();
 initServo();
@@ -17,17 +16,39 @@ initServo();
 while(1)
 {
 	lresult = readADC(2); //read from ADC channel 2 for the left channel
-//	rresult = readADC(3); //read from ADC channel 3 for the right channel
+	rresult = readADC(3); //read from ADC channel 3 for the right channel
 
-	if (lresult >= 920) //~4.5 volts -- no object detected so keep moving
-		OCR1A = 436;
-		else if (lresult >= 700) //~3.5 to 4.5 volts, object is close, so slow down
-			OCR1A = 400;
-		else	
-			OCR1A = 386; //object detected - stop
+	if (lresult >= 900) //no object on the left side
+	{
+		if (rresult >=900) //no object on the right side, keep moving forward
+		{	
+			OCR1A=436;
+			OCR1B=436;
+		}
+		else				//object detected on right side so stop right wheel turn hard left
+		{	
+			OCR1A=436;
+			OCR1B=386;
+		}	
+	}
 	
+	else 				//object on the left side
+	{
+		if (rresult >=900) //no object on the right side, stop left wheel turn hard right
+		{	
+			OCR1A=386;
+			OCR1B=436;
+		}
+		else				//object on both sides; stop until a path clears
+		{	
+			OCR1A=386;
+			OCR1B=386;
+		}	
+	}
+	
+		
 	_delay_ms(500); //wait half a second before taking next reading 
 	
-}
+} //end of while(1) loop
 
 } //end of program
