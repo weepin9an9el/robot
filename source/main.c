cbb00351076a5dbe415e4c8667910354c168f496
weepin9an9el <weepin9an9el@gmail.com>
2017-02-28 19:57:23 -0600

    For each ADC reading and comparison, make a decision on what direction to travel and then do it for 250 msec.. then wait half a second and take another reading

--- source/main.c
+++ source/main.c
@@ -8,27 +8,39 @@ void main(void)
 {
 int lresult;
 int rresult;
+int i;
 
 initADC();
 initServo();
 //Uncomment the initLCD function below if you want LCD support
 //initLCD();
+
+_delay_ms(500); //just a startup delay
+
 while(1)
 {
 	lresult = readADC(2); //read from ADC channel 2 for the left channel
 	rresult = readADC(3); //read from ADC channel 3 for the right channel
-
+	
 	if (lresult >= 900) //no object on the left side
 	{
 		if (rresult >=900) //no object on the right side, keep moving forward
 		{	
-			OCR1A=436;
-			OCR1B=436;
+			for (i=250; i>0; i--)
+			{
+				OCR1A=436;
+				OCR1B=436;
+				_delay_ms(1);
+			}
 		}
 		else				//object detected on right side so stop right wheel turn hard left
 		{	
-			OCR1A=436;
-			OCR1B=386;
+			for (i=250; i>0; i--)
+			{
+				OCR1A=436;
+				OCR1B=386;
+				_delay_ms(1);
+			}
 		}	
 	}
 	
@@ -36,17 +48,24 @@ while(1)
 	{
 		if (rresult >=900) //no object on the right side, stop left wheel turn hard right
 		{	
-			OCR1A=386;
-			OCR1B=436;
+			for (i=250; i>0; i--)
+			{
+				OCR1A=386;
+				OCR1B=436;
+				_delay_ms(1);
+			}
 		}
 		else				//object on both sides; stop until a path clears
 		{	
-			OCR1A=386;
-			OCR1B=386;
+			for (i=250; i>0; i--)
+			{
+				OCR1A=386;
+				OCR1B=386;
+				_delay_ms(1);
+			}
 		}	
 	}
 	
-		
 	_delay_ms(500); //wait half a second before taking next reading 
 	
 } //end of while(1) loop
